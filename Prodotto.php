cfcfbd2c7df4f3f642338692b4500fc8df23fd93
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-20 16:07:12 +0200

    First commit

---
+++ Prodotto.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Prodotto {
+
+        public function __construct()
+        {
+            
+        }
+
+        private $name = "";
+        private $description = "";
+        private $cost = 0;
+        private $weight = 0;
+
+    }
+
+
+?>
